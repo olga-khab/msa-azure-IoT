fca173a1fc3dc0a1b7537dcad03885a6f5cfb38b
olga-khab <olga.a.khab@gmail.com>
2020-07-13 15:16:35 +1000

    added coordinates and distance, removed emissions

--- iot-device-simulation.py
+++ iot-device-simulation.py
@@ -1,41 +1,47 @@
 import random
 import time
-
-# we will simulate a car
+from math import cos, sin, asin, sqrt, pi
 
 from azure.iot.device import IoTHubDeviceClient, Message
 CONNECTION_STRING = "HostName=msa-iot-hub.azure-devices.net;DeviceId=iot-device;SharedAccessKey=4q8VLNeau4Xg+K9NHzVzf7bDHSwuugIVE+HTNmxkWIc="
 
 # measurements transmitted by device - set default values
 SPEED = 60 # km/h
-#LATITUDE =
-#LONGITUDE =
-ENGINE_TEMP = 20
-EMISSIONS = 20
-MESSAGE = '{{"speed": {speed},"engine temperature": {engine_temperature}, "emissions": {emissions}}}'
+# startung coordinates in Sydney
+LATITUDE = -33.861262
+LONGITUDE = 151.180457
+# standard car engine temperature is between 90-105 degrees Celcius
+ENGINE_TEMP = 95
+DISTANCE = 0
+MESSAGE = '{{"speed": {speed},"engine temperature": {engine_temperature}, "distance": {distance}, "latitude": {lat}, "longitude": {long}}}'
+
+# calculate distance between two locations
+def haversine_distance(lat1, long1, lat2, long2):
+    a = 0.5 - cos((lat2-lat1)*pi/180)/2 + cos(lat1*pi/180) * cos(lat2*pi/180) * (1-cos((long2-long1)*pi/180))/2
+    return 2*6371*asin(sqrt(a))
 
 # authenitication string to connect to the IoT Hub
 def iothub_client_telemetry_sample_run():
+    lat = LATITUDE
+    long = LONGITUDE
+    distance = DISTANCE
     try:
         client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-        #print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
-
         while True:
             # simulate telemetry values to be transmitted
-            speed = SPEED + (random.random() * 10)
-            engine_temperature = ENGINE_TEMP + (random.random() * 10)
-            emissions = EMISSIONS + (random.random() * 10)
+            speed = SPEED + random.random()*50
+            # make engine tempertaure a function of speed
+            engine_temperature = ENGINE_TEMP + random.random()*speed
+            # simulate movement by changing location
+            lat = lat + random.random()*0.00001
+            long = long + random.random()*0.00001
+            # calculate distance travelled
+            distance = distance + haversine_distance(LATITUDE, LONGITUDE, lat, long)
+
             # format the message
             message = Message(MESSAGE.format(speed = speed,
-            engine_temperature=engine_temperature, emissions = emissions))
-
-            # Add a custom application property to the message.
-            # An IoT hub can filter on these properties without access to the message body.
-            #if temperature > 30:
-            #  message.custom_properties["temperatureAlert"] = "true"
-            #else:
-            #  message.custom_properties["temperatureAlert"] = "false"
-
+            engine_temperature=engine_temperature, lat = lat,
+            long = long, distance=distance))
             # send message every 5 seconds
             print( "Sending message: {}".format(message) )
             client.send_message(message)
@@ -46,16 +52,6 @@ def iothub_client_telemetry_sample_run():
         print ( "IoTHubClient sample stopped" )
 
 if __name__ == '__main__':
-#    print ( "IoT Hub Quickstart #1 - Simulated device" )
-#    print ( "Press Ctrl-C to exit" )
+    print ( "IoT Hub - simulated car device" )
+    print ( "Press Ctrl-C to exit" )
     iothub_client_telemetry_sample_run()
-
-'''
-import googlemaps
-gmaps = googlemaps.Client(key='AIzaSyCOg0z_8A31ZZP6uE9JMk5iZsRRFyz3ilg')
-start = 'Constitution Ave NW & 10th St NW, Washington, DC'
-end   = 'Independence and 6th SW, Washington, DC 20024, USA'
-dirs  = gmaps.directions(start, end)[0]['legs'][0]['steps']
-print(dirs)
-
-'''
